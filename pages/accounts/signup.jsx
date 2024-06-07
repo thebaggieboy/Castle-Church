2aac1fb6e222509ca378393287534fd5148c0656
Mofe Odujirin <thebaggieboy@protonmail.com>
2024-06-08 00:40:17 +0100

    ss

--- pages/accounts/signup.jsx
+++ pages/accounts/signup.jsx
@@ -7,13 +7,10 @@ export default function signup() {
     <section class="bg-gray-50 dark:bg-gray-900 p-2">
     
   <div class="flex flex-col items-center justify-center mx-auto md:h-screen lg:py-0">
-  <Link href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-        {/* {  <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/>} */}
-          CASTLE    
-      </Link>
+
       <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
       <div className="text-center">
-       <img className='text-center' src="/images/logo.jpg" style={{width:120, textAlign:'center', alignItems:'center'}} alt="" />
+      
        </div>
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
@@ -42,7 +39,7 @@ export default function signup() {
                   </div>
                   <button type="submit" class="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
                   <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Already have an account? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
+                      Already have an account? <Link href="/accounts/login" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</Link>
                   </p>
               </form>
           </div>
