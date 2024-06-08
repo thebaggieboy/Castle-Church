6a2fc73fe58951e36c24c11693a3a6a96801326a
Mofe Odujirin <thebaggieboy@protonmail.com>
2024-06-08 01:15:17 +0100

    s

--- pages/admin/user/index.jsx
+++ pages/admin/user/index.jsx
@@ -459,7 +459,7 @@ const USERS = 'SELECT * FROM user_tab WHERE '
         <h4 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-2xl dark:text-white">You are about to delete this user profile, do you want to continue?</h4>
   
         <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-            <a href="/" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+            <a href="/" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-red-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                 Yes, i want to continue
                 <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
@@ -475,8 +475,8 @@ const USERS = 'SELECT * FROM user_tab WHERE '
           </div>
         </Modal.Body>
         <Modal.Footer>
-        <Button className='bg-blue-800 text-white' onClick={() => setOpenModal2(false)}>
-            Update
+        <Button className='bg-red-800 text-white' onClick={() => setOpenModal2(false)}>
+            Delete
           </Button>
 
           <Button color="gray" onClick={() => setOpenModal2(false)}>
